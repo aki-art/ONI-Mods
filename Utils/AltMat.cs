baaa569b7c9503d252d30da390c6df3b81a2f240
akitary <akitary.commissions@gmail.com>
2020-07-20 04:11:07 +0200

    .

---
+++ Utils/AltMat.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utils
+{
+    public class AlternativeMaterial
+    {
+        public string def;
+        public string parentDef;
+        public Tag element;
+
+        public AlternativeMaterial(string def, string parentDef, Tag element)
+        {
+            this.def = def ?? throw new ArgumentNullException(nameof(def));
+            this.parentDef = parentDef ?? throw new ArgumentNullException(nameof(parentDef));
+            this.element = element;
+        }
+    }
+    class AltMat
+    {
+        public static List<AlternativeMaterial> alternativeMats;
+
+        
+    }
+}
