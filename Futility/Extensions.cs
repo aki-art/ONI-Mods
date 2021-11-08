605524f4ef8b9524e967ba4569997c63f977e1d3
akitary.art@gmail.com <aki.senkinn@gmail.com>
2021-11-08 20:45:57 +0100

    TMP stuff

---
+++ Futility/Extensions.cs
@@ -0,0 +1,53 @@
+﻿using ProcGen;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FUtility
+{
+    public static class Extensions
+    {
+        public static void AddAll<T>(this IEnumerable<T> enumerator, params T[] items)
+        {
+            AddAll(enumerator, items.AsEnumerable());
+        }
+
+        public static void AddAll<T>(this IEnumerable<T> enumerator, IEnumerable<T> items)
+        {
+            foreach (T item in items)
+            {
+                enumerator.Append(item);
+            }
+        }
+
+        public static bool IsNullOrEmpty<T>(this IEnumerable<T> enumerator)
+        {
+            return enumerator != null && enumerator.Count() > 0;
+        }
+
+        public static T GetWeightedRandom<T>(this IEnumerable<T> enumerator, SeededRandom rand = null) where T : IWeighted
+        {
+            if (enumerator.IsNullOrEmpty()) return default;
+            float totalWeight = enumerator.Sum(n => n.weight);
+            float treshold = rand == null ? UnityEngine.Random.value : rand.RandomValue();
+            treshold *= totalWeight;
+
+            float num3 = 0.0f;
+            foreach (T item in enumerator)
+            {
+                num3 += item.weight;
+                if (num3 > treshold)
+                    return item;
+            }
+
+            return enumerator.GetEnumerator().Current;
+        }
+
+        public static T GetRandomAny<T>(this IEnumerable<T> enumerator, SeededRandom rand = null)
+        {
+            if (enumerator.IsNullOrEmpty()) return default;
+            int index = rand == null ? UnityEngine.Random.Range(0, enumerator.Count()) : rand.RandomRange(0, enumerator.Count());
+
+            return enumerator.ElementAt(index);
+        }
+    }
+}
---
+++ Futility/FUI/TMPFixer.cs
@@ -0,0 +1,21 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace FUtility.FUI
+{
+    class TMPFixer : KMonoBehaviour
+    {
+        [SerializeField]
+        public TextAlignmentOptions alignment;
+
+        [MyCmpReq]
+        private LocText text;
+
+        protected override void OnSpawn()
+        {
+            base.OnSpawn();
+            text.alignment = alignment;
+            Destroy(this);
+        }
+    }
+}
