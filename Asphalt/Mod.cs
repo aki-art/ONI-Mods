636c9f07d03d14321e836a25942835b68c60edfc
AkiSenkinn <aki.senkinn@gmail.com>
2021-12-25 09:27:02 +0100

    Auto Sweeper fix

--- Asphalt/Mod.cs
+++ Asphalt/Mod.cs
@@ -3,6 +3,7 @@ using FUtility;
 using FUtility.SaveData;
 using HarmonyLib;
 using KMod;
+using System.Collections.Generic;
 
 namespace Asphalt
 {
@@ -11,17 +12,20 @@ namespace Asphalt
         public const string ID = "Asphalt";
         public static string modPath;
         public static SaveDataManager<Config> config;
-
         public static Config Settings => config.Settings;
 
         public override void OnLoad(Harmony harmony)
         {
             base.OnLoad(harmony);
-            Log.Debuglog("DEBUG");
             Log.PrintVersion();
             ModAssets.LoadAssets();
             modPath = path;
             config = new SaveDataManager<Config>(path);
+        }
+
+        public override void OnAllModsLoaded(Harmony harmony, IReadOnlyList<KMod.Mod> mods)
+        {
+            base.OnAllModsLoaded(harmony, mods);
         }
     }
 }
--- Asphalt/Patches/ElementLoaderPatch.cs
+++ Asphalt/Patches/ElementLoaderPatch.cs
@@ -1,28 +1,48 @@
 ﻿using FUtility;
 using HarmonyLib;
 using System.Collections.Generic;
+using UnityEngine;
 
 namespace Asphalt.Patches
 {
     // Prepare Bitumen to be introduced to actual gameplay
-    class ElementLoaderPatch
+    public class ElementLoaderPatch
     {
         [HarmonyPatch(typeof(ElementLoader), "Load")]
         public static class Patch_ElementLoader_Load
         {
+            private static void AddTag(ref Tag[] tags, Tag tag)
+            {
+                // dont add duplicate tags (in case another mod added stuff before me)
+                foreach (Tag t in tags)
+                {
+                    if (t == tag) return;
+                }
+
+                tags = tags.AddToArray(tag);
+            }
+            private static Tag CreateMaterialCategoryTag(Tag phaseTag, string materialCategoryField)
+            {
+                if (string.IsNullOrEmpty(materialCategoryField))
+                    return phaseTag;
+
+                return TagManager.Create(materialCategoryField);
+            }
+
             public static void Postfix(Dictionary<string, SubstanceTable> substanceTablesByDlc)
             {
                 Element bitumen = ElementLoader.FindElementByHash(SimHashes.Bitumen);
-                bitumen.materialCategory = GameTags.ManufacturedMaterial;
+                //bitumen.materialCategory = GameTags.ManufacturedMaterial;
+                bitumen.materialCategory = CreateMaterialCategoryTag(TagManager.Create("Solid"), GameTags.ManufacturedMaterial.ToString()); // This tag is for storage
 
                 if (bitumen.oreTags is null)
                 {
                     bitumen.oreTags = new Tag[] { };
                 }
 
-                bitumen.oreTags.AddToArray(GameTags.ManufacturedMaterial);
-                bitumen.oreTags.AddToArray(GameTags.BuildableAny);
-                bitumen.oreTags.AddToArray(GameTags.Solid);
+                AddTag(ref bitumen.oreTags, GameTags.ManufacturedMaterial);
+                AddTag(ref bitumen.oreTags, GameTags.BuildableAny);
+                AddTag(ref bitumen.oreTags, GameTags.Solid);
 
                 if (bitumen.substance is null)
                 {
@@ -30,11 +50,16 @@ namespace Asphalt.Patches
                     return;
                 }
 
+                bitumen.substance = ModUtil.CreateSubstance(
+                    "bitumen",
+                    Element.State.Solid,
+                    Assets.Anims.Find(anim => anim.name == "solid_bitumen_kanim"),
+                    new Material(bitumen.substance.material),
+                    ModAssets.Colors.bitumen,
+                    ModAssets.Colors.bitumen,
+                    ModAssets.Colors.bitumen );
+
                 bitumen.substance.material.mainTexture = ModAssets.bitumenTexture;
-                bitumen.substance.anim = Assets.Anims.Find(anim => anim.name == "solid_bitumen_kanim");
-                bitumen.substance.colour = ModAssets.Colors.bitumen;
-                bitumen.substance.conduitColour = ModAssets.Colors.bitumen;
-                bitumen.substance.uiColour = ModAssets.Colors.bitumen;
             }
         }
     }
