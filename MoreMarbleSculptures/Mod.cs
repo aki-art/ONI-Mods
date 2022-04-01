9c214fa7d23d386d195064ac9e20c6c1e5e7325a
AkiSenkinn <aki.senkinn@gmail.com>
2022-04-01 22:15:20 +0200

    More marbles

---
+++ MoreMarbleSculptures/Mod.cs
@@ -0,0 +1,24 @@
+﻿using FUtility;
+using FUtility.SaveData;
+using HarmonyLib;
+using KMod;
+using MoreMarbleSculptures.Settings;
+
+namespace MoreMarbleSculptures
+{
+    public class Mod : UserMod2
+    {
+        public static SaveDataManager<Config> config;
+
+        public static Config Settings => config.Settings;
+
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+
+            Log.PrintVersion();
+
+            config = new SaveDataManager<Config>(path);
+        }
+    }
+}
---
+++ MoreMarbleSculptures/Settings/Config.cs
@@ -0,0 +1,14 @@
+﻿using FUtility.SaveData;
+using System.Collections.Generic;
+
+namespace MoreMarbleSculptures.Settings
+{
+    public class Config : IUserSetting
+    {
+        public Dictionary<string, Artable.Status> MoveSculptures { get; set; } = new Dictionary<string, Artable.Status>()
+        {
+            { "Average", Artable.Status.Great }, // Unicorn
+            { "Bad", Artable.Status.Okay } // Mushroom
+        };
+    }
+}
