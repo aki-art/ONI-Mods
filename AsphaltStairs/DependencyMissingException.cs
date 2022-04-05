cce442a6319f48171d78c272f10b8f627b048aff
AkiSenkinn <aki.senkinn@gmail.com>
2022-04-05 05:04:53 +0200

    Dependency error handling

---
+++ AsphaltStairs/DependencyMissingException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AsphaltStairs
+{
+    public class DependencyMissingException : Exception
+    {
+        public DependencyMissingException(string name) : base(GetMessage(name))
+        {
+        }
+
+        private static string GetMessage(string name)
+        {
+            return string.Format("Dependency missing: \"{0}\". Please install all dependencies and enable them in the Mod menu.", name);
+        }
+    }
+}
--- AsphaltStairs/Mod.cs
+++ AsphaltStairs/Mod.cs
@@ -1,6 +1,9 @@
-﻿using HarmonyLib;
+﻿using FUtility;
+using HarmonyLib;
 using KMod;
+using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace AsphaltStairs
 {
@@ -8,12 +11,33 @@ namespace AsphaltStairs
     {
         public static readonly Tag stairsTag = TagManager.Create("Stairs");
 
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+        }
+
         public override void OnAllModsLoaded(Harmony harmony, IReadOnlyList<KMod.Mod> mods)
         {
+            CheckIfStairsModIsHere(mods);
+
             base.OnAllModsLoaded(harmony, mods);
 
             Integration.Asphalt.TrySetSpeedModifier();
             Integration.Stairs.TryPatch(harmony);
+        }
+
+        private void CheckIfStairsModIsHere(IReadOnlyList<KMod.Mod> mods)
+        {
+            string stairsID = "Stairs";
+
+            var stairs = mods.First(m => m.staticID == stairsID);
+            if (stairs == null || !stairs.IsActive())
+            {
+                Log.Warning($"This mod will now force disable itself to let you restart the game without a crash.");
+                mod.SetEnabledForActiveDlc(false);
+                 
+                throw new DependencyMissingException(stairsID);
+            }
         }
     }
 }
