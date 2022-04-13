32916eddd62222710e2ed782e2765b6febcb34af
AkiSenkinn <aki.senkinn@gmail.com>
2022-04-13 02:46:15 +0200

    Fixed printable bg colors not appearing on first open

--- PrintingPodRecharge/Patches/ImmigrantScreenPatch.cs
+++ PrintingPodRecharge/Patches/ImmigrantScreenPatch.cs
@@ -51,7 +51,7 @@ namespace PrintingPodRecharge.Patches
         {
             public static void Postfix(CarePackageContainer __instance)
             {
-                TintBG(__instance, "Details/PortraitContainer/BG");
+                GameScheduler.Instance.ScheduleNextFrame("", obj => TintBG(__instance, "Details/PortraitContainer/BG"));
             }
         }
 
@@ -60,7 +60,7 @@ namespace PrintingPodRecharge.Patches
         {
             public static void Postfix(CharacterContainer __instance)
             {
-                TintBG(__instance, "Details/Top/PortraitContainer/BG");
+                GameScheduler.Instance.ScheduleNextFrame("", obj => TintBG(__instance, "Details/Top/PortraitContainer/BG"));
             }
         }
 
@@ -76,10 +76,9 @@ namespace PrintingPodRecharge.Patches
 
             kbac.SwapAnims(new KAnimFile[] { Assets.GetAnim("rpp_greyscale_dupeselect_kanim") });
 
-            var color = UnityEngine.Random.ColorHSV();
-            kbac.SetSymbolTint("forever", color);
-            kbac.SetSymbolTint("grid_bloom", color);
-            kbac.SetSymbolTint("inside_rough", color);
+            kbac.SetSymbolTint("forever", ImmigrationModifier.Instance.bgColor);
+            kbac.SetSymbolTint("grid_bloom", ImmigrationModifier.Instance.glowColor);
+            kbac.SetSymbolTint("inside_rough", ImmigrationModifier.Instance.glowColor);
 
             kbac.SetDirty();
             kbac.UpdateAnim(1);
