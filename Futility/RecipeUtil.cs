5b207721d492fdfaa7dabe60e97cdecc0c6c7fda
AkiSenkinn <aki.senkinn@gmail.com>
2022-06-14 00:02:38 +0200

    tweak recipe builder

--- Futility/RecipeUtil.cs
+++ Futility/RecipeUtil.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using static ComplexRecipe;
+using static ComplexRecipe.RecipeElement;
 
 namespace FUtility
 {
@@ -34,15 +35,21 @@ namespace FUtility
             return this;
         }
 
-        public RecipeBuilder Input(Tag tag, float amount)
+        public RecipeBuilder Input(Tag tag, float amount, bool inheritElement = true)
         {
-            inputs.Add(new RecipeElement(tag, amount));
+            inputs.Add(new RecipeElement(tag, amount, inheritElement));
             return this;
         }
 
-        public RecipeBuilder Output(Tag tag, float amount)
+        public RecipeBuilder Output(Tag tag, float amount, TemperatureOperation tempOp = TemperatureOperation.AverageTemperature)
         {
-            outputs.Add(new RecipeElement(tag, amount));
+            outputs.Add(new RecipeElement(tag, amount, tempOp));
+            return this;
+        }
+
+        public RecipeBuilder FacadeOutput(Tag tag, float amount, string facadeID = "", bool storeElement = false, TemperatureOperation tempOp = TemperatureOperation.AverageTemperature)
+        {
+            outputs.Add(new RecipeElement(tag, amount, tempOp, facadeID, storeElement));
             return this;
         }
 
