b3352c4d7e242fa1944604bb75f8f1065ba6bf41
AkiSenkinn <aki.senkinn@gmail.com>
2022-07-10 09:35:05 +0200

    Complete Element Textures project added
    # Conflicts:
    #	ONIMods.sln

---
+++ CompleteElementTextures/Mod.cs
@@ -0,0 +1,15 @@
+﻿using FUtility;
+using HarmonyLib;
+using KMod;
+
+namespace CompleteElementTextures
+{
+    public class Mod : UserMod2
+    {
+        public override void OnLoad(Harmony harmony)
+        {
+            base.OnLoad(harmony);
+            Log.PrintVersion();
+        }
+    }
+}
