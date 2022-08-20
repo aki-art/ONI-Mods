e4ab014479c5c5b27b21d522a4b773b6897f1a72
AkiSenkinn <aki.senkinn@gmail.com>
2022-08-20 21:14:51 +0200

    Crispy Fire Cracklings

---
+++ Slag/Content/Items/CrispyFireCracklingsConfig.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+
+namespace Slag.Content.Items
+{
+    public class CrispyFireCracklingsConfig : IEntityConfig
+    {
+        public const string ID = "Slag_CrispyFireCracklings";
+        // public static ComplexRecipe recipe;
+
+        public GameObject CreatePrefab()
+        {
+            var prefab = EntityTemplates.CreateLooseEntity(
+                ID,
+                STRINGS.ITEMS.FOOD.CRISPY_CRACKLINGS.NAME,
+                STRINGS.ITEMS.FOOD.CRISPY_CRACKLINGS.DESC,
+                1f,
+                false,
+                Assets.GetAnim("cottoncandy_kanim"),
+                "object",
+                Grid.SceneLayer.Front,
+                EntityTemplates.CollisionShape.RECTANGLE,
+                0.8f,
+                0.4f,
+                true);
+
+            var foodInfo = new EdiblesManager.FoodInfo(
+                 ID,
+                 DlcManager.VANILLA_ID,
+                 2000f * 1000f,
+                 3,
+                 255.15f,
+                 277.15f,
+                 600f,
+                 true);
+
+            EntityTemplates.ExtendEntityToFood(prefab, foodInfo);
+
+            return prefab;
+        }
+
+        public string[] GetDlcIds()
+        {
+            return DlcManager.AVAILABLE_ALL_VERSIONS;
+        }
+
+        public void OnPrefabInit(GameObject inst)
+        {
+        }
+
+        public void OnSpawn(GameObject inst)
+        {
+        }
+    }
+}
---
+++ Slag/Patches/EdiblePatch.cs
@@ -0,0 +1,30 @@
+﻿using FUtility;
+using HarmonyLib;
+using Slag.Content.Items;
+
+namespace Slag.Patches
+{
+    public class EdiblePatch
+    {
+        [HarmonyPatch(typeof(Edible), "StopConsuming")]
+        public class Edible_StopConsuming_Patch
+        {
+            public static void Postfix(Edible __instance, Worker worker)
+            {
+                if(__instance.PrefabID() == CrispyFireCracklingsConfig.ID)
+                {
+                    Log.Debuglog("Eaten crispy fire");
+                    new EmoteChore(
+                        worker.GetComponent<ChoreProvider>(), 
+                        Db.Get().ChoreTypes.EmoteHighPriority, 
+                        "anim_react_relish_kanim", 
+                        new HashedString[]
+                        {
+                            "react"
+                        }, 
+                        null);
+                }
+            }
+        }
+    }
+}
