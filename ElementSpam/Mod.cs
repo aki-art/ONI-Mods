13b31680f38414c72608173183d666b6af1bcedb
AkiSenkinn <aki.senkinn@gmail.com>
2022-11-01 16:19:05 +0100

    .

--- ElementSpam/Mod.cs
+++ ElementSpam/Mod.cs
@@ -1,15 +1,65 @@
 ﻿using FUtility;
 using HarmonyLib;
 using KMod;
+using System.Collections.Generic;
+using UnityEngine;
 
 namespace ElementSpam
 {
     public class Mod : UserMod2
     {
-        public override void OnLoad(Harmony harmony)
+        public const int COUNT = 80;
+        internal static List<Substance> substanceList;
+        public static List<SimHashes> simHashes = new List<SimHashes>();
+        internal static AccessTools.FieldRef<SubstanceTable, List<Substance>> ref_substanceList;
+
+        [HarmonyPatch(typeof(ElementLoader), "CollectElementsFromYAML")]
+        public class ElementLoader_CollectElementsFromYAML_Patch
         {
-            base.OnLoad(harmony);
-            Log.PrintVersion();
+            public static void Postfix(ref List<ElementLoader.ElementEntry> __result)
+            {
+                var elementList = new List<ElementLoader.ElementEntry>();
+
+                for (var i = 0; i < COUNT; i++)
+                {
+                    //Log.Debuglog("Adding element " + i);
+                    var local = "STRINGS.ELEMENTS.TESTS.TEST_" + i;
+                    var name = "Test " + i;
+                    Strings.Add(local, name);
+
+                    var newElement = new ElementLoader.ElementEntry
+                    {
+                        elementId = "TestElement" + i,
+                        state = Element.State.Liquid,
+                        dlcId = DlcManager.VANILLA_ID,
+                        localizationID = local,
+                        isDisabled = false
+                    };
+
+                    simHashes.Add(EnumPatch.RegisterSimHash(name));
+                    elementList.Add(newElement);
+                };
+
+                __result.AddRange(elementList);
+
+                var animFile = Assets.Anims.Find(a => a.name == "liquid_tank_kanim");
+
+                foreach (var item in simHashes)
+                {
+                    var color = Random.ColorHSV();
+                    var substance = ModUtil.CreateSubstance(item.ToString(), Element.State.Liquid, animFile, Assets.instance.substanceTable.liquidMaterial, color, color, color);
+                    substanceList.Add(substance);
+                }
+            }
+
+            [HarmonyPatch(typeof(ElementLoader), "Load")]
+            public class ElementLoader_Load_Patch
+            {
+                public static void Prefix(Dictionary<string, SubstanceTable> substanceTablesByDlc)
+                {
+                    substanceList = Traverse.Create(substanceTablesByDlc[DlcManager.VANILLA_ID]).Field<List<Substance>>("list").Value;
+                }
+            }
         }
     }
 }
