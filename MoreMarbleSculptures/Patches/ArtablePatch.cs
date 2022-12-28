012700113a534eb427ba2ff9b1deb6f56e04c5e1
AkiSenkinn <aki.senkinn@gmail.com>
2022-12-28 01:14:21 +0100

    load fix

--- MoreMarbleSculptures/Patches/ArtablePatch.cs
+++ MoreMarbleSculptures/Patches/ArtablePatch.cs
@@ -1,4 +1,5 @@
-﻿using FUtilityArt.Components;
+﻿using FUtility;
+using FUtilityArt.Components;
 using HarmonyLib;
 
 namespace MoreMarbleSculptures.Patches
@@ -9,23 +10,27 @@ namespace MoreMarbleSculptures.Patches
         [HarmonyPatch(typeof(Artable), "OnSpawn")]
         public class Artable_OnSpawn_Patch
         {
-            public static void Prefix(Artable __instance, ref string ___currentStage)
+            public static void Prefix(Artable __instance, ref string ___currentStage, string ___defaultArtworkId)
             {
                 if (__instance.TryGetComponent(out ArtOverride artOverride) && !artOverride.overrideStage.IsNullOrWhiteSpace())
                 {
-                    ___currentStage = artOverride.overrideStage;
-                }
-            }
-        }
+                    var newStage = artOverride.overrideStage;
 
-        [HarmonyPatch(typeof(Artable), "OnDeserialized")]
-        public class Artable_OnDeserialized_Patch
-        {
-            // prevent invalid stages
-            public static void Postfix(string ___defaultArtworkId, ref string ___currentStage)
-            {
+                    if (Db.GetArtableStages().TryGet(newStage) == null)
+                    {
+                        Log.Info(newStage + " was not found, trying to upgrade");
+                        newStage = MarbleSculptureConfig.ID + "_" + newStage;
+                    }
+
+                    if (Db.GetArtableStages().TryGet(newStage) != null)
+                    {
+                        ___currentStage = newStage;
+                    }
+                }
+
                 if (Db.GetArtableStages().TryGet(___currentStage) == null)
                 {
+                    Log.Info(___currentStage + " was still not found, resetting to default");
                     ___currentStage = ___defaultArtworkId;
                 }
             }
