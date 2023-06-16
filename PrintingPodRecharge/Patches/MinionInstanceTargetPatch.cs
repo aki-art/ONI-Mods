193f67ac1c827674225e363d29a90feae7cfa3ff
Aki Senkinn <akitary.art@gmail.com>
2023-06-16 13:09:33 +0200

    undo the crash

--- PrintingPodRecharge/Patches/MinionInstanceTargetPatch.cs
+++ PrintingPodRecharge/Patches/MinionInstanceTargetPatch.cs
@@ -1,12 +1,14 @@
-﻿using FUtility;
-using HarmonyLib;
+﻿using HarmonyLib;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
 using UnityEngine;
 
 namespace PrintingPodRecharge.Patches
 {
 	public class MinionInstanceTargetPatch
 	{
-		[HarmonyPatch(typeof(MinionBrowserScreen.GridItem.MinionInstanceTarget), MethodType.Constructor)]
+/*		[HarmonyPatch(typeof(MinionBrowserScreen.GridItem.MinionInstanceTarget), MethodType.Constructor)]
 		public class MinionInstanceTarget_Ctor_Patch
 		{
 			public static void Prefix(MinionBrowserScreen.GridItem.MinionInstanceTarget __instance)
@@ -17,28 +19,35 @@ namespace PrintingPodRecharge.Patches
 					__instance.personality = Db.Get().Personalities.GetRandom(true, false);
 				}
 			}
-		}
+		}*/
 
-		// temporary fix that is very unfriendly to other mods
-		[HarmonyPatch(typeof(MinionBrowserScreen.GridItem), "Of")]
+		// temporary bandaid fix
+		[HarmonyPatch(typeof(MinionBrowserScreen.GridItem), "Of", typeof(GameObject))]
 		public class MinionBrowserScreen_GridItem_Of_Patch
 		{
-			public static bool Prefix(ref MinionBrowserScreen.GridItem.MinionInstanceTarget __result, GameObject minionInstance)
+			public static IEnumerable<CodeInstruction> Transpiler(ILGenerator _, IEnumerable<CodeInstruction> orig)
 			{
-				var component = minionInstance.GetComponent<MinionIdentity>();
+				var codes = orig.ToList();
+
+				var p_personalityResourceId = typeof(MinionIdentity).GetProperty("personalityResourceId").GetGetMethod();
+				var index = codes.FindIndex(ci => ci.Calls(p_personalityResourceId));
+
+				if (index == -1)
+					return codes;
+
+				var m_InjectedMethod = AccessTools.DeclaredMethod(typeof(MinionBrowserScreen_GridItem_Of_Patch), nameof(ValidatePersonalityId));
+
+				codes.Insert(index + 1, new CodeInstruction(OpCodes.Call, m_InjectedMethod));
+
+				return codes;
+			}
+
+			private static HashedString ValidatePersonalityId(HashedString personalityId)
+			{
 				var personalities = Db.Get().Personalities;
-
-				var personality = personalities.TryGet(component.personalityResourceId)
-					?? personalities.GetRandom(true, false);
-
-				__result = new MinionBrowserScreen.GridItem.MinionInstanceTarget()
-				{
-					minionInstance = minionInstance,
-					minionIdentity = component,
-					personality = personality
-				};
-
-				return false;
+				return personalities.TryGet(personalityId) == null
+					? personalities.GetRandom(true, false).IdHash
+					: personalityId;
 			}
 		}
 	}
