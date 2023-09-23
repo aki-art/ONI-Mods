94f10e3adba618a2bbb2b104a0340f2e014ef23e
Aki Senkinn <akitary.art@gmail.com>
2023-09-23 10:29:43 +0200

    my friends dupe died mysteriously so i made a mod to revive him

---
+++ ReviveADupe/Mod.cs
@@ -0,0 +1,26 @@
+﻿using FUtility;
+using HarmonyLib;
+using KMod;
+using UnityEngine;
+
+namespace ReviveADupe
+{
+	public class Mod : UserMod2
+	{
+		public override void OnLoad(Harmony harmony)
+		{
+			base.OnLoad(harmony);
+			Log.PrintVersion(this);
+		}
+
+		[HarmonyPatch(typeof(MinionConfig), "CreatePrefab")]
+		public class MinionConfig_CreatePrefab_Patch
+		{
+			public static void Postfix(GameObject __result)
+			{
+				__result.AddComponent<ReviveADupe_ReviveableCorpse>();
+			}
+		}
+
+	}
+}
---
+++ ReviveADupe/ReviveADupe_ReviveableCorpse.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+namespace ReviveADupe
+{
+	public class ReviveADupe_ReviveableCorpse : KMonoBehaviour, ISidescreenButtonControl
+	{
+		[MySmiReq] private DeathMonitor.Instance deathMonitor;
+
+		protected override void OnSpawn()
+		{
+			base.OnSpawn();
+			GetComponent<DeathMonitor>();
+		}
+
+		public string SidescreenButtonText => "Revive";
+
+		public string SidescreenButtonTooltip => "";
+
+		public int ButtonSideScreenSortOrder() => 0;
+
+		public int HorizontalGroupID() => -1;
+
+		public void OnSidescreenButtonPressed()
+		{
+			var go = new GameObject("temporary carrier");
+			var storage = go.AddComponent<MinionStorage>();
+			var pos = transform.position;
+			storage.SerializeMinion(gameObject);
+			storage.DeserializeMinion(storage.GetStoredMinionInfo()[0].id, pos);
+
+			Destroy(go);
+		}
+
+		public void SetButtonTextOverride(ButtonMenuTextOverride textOverride)
+		{
+		}
+
+		public bool SidescreenButtonInteractable() => true;
+
+		public bool SidescreenEnabled() => deathMonitor.IsDead();
+	}
+}
