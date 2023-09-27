258e707ad34d945c0442f02ed58b65e969bbb093
Aki Senkinn <akitary.art@gmail.com>
2023-09-27 07:43:12 +0200

    no name limit start

---
+++ NoNameLengthLimit/Mod.cs
@@ -0,0 +1,25 @@
+﻿using FUtility;
+using HarmonyLib;
+using KMod;
+
+namespace NoNameLengthLimit
+{
+	public class Mod : UserMod2
+	{
+		public override void OnLoad(Harmony harmony)
+		{
+			base.OnLoad(harmony);
+			Log.PrintVersion(this);
+		}
+
+
+		[HarmonyPatch(typeof(EditableTitleBar), "OnSpawn")]
+		public class EditableTitleBar_OnSpawn_Patch
+		{
+			public static void Postfix(EditableTitleBar __instance)
+			{
+				__instance.inputField.characterLimit = 1024;
+			}
+		}
+	}
+}
