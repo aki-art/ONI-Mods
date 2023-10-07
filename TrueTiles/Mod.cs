ea106b125519364c3fba39ed7b09edc2d179a397
Aki Senkinn <akitary.art@gmail.com>
2023-10-07 20:50:36 +0200

    Futility update

--- TrueTiles/Mod.cs
+++ TrueTiles/Mod.cs
@@ -12,70 +12,70 @@ using static Rendering.BlockTileRenderer;
 
 namespace TrueTiles
 {
-    public class Mod : UserMod2
-    {
-        private static SaveDataManager<Config> config;
-        public static Harmony harmonyInstance;
+	public class Mod : UserMod2
+	{
+		private static SaveDataManager<Config> config;
+		public static Harmony harmonyInstance;
 
-        public static Config Settings => config.Settings;
+		public static Config Settings => config.Settings;
 
-        public static string GetExternalSavePath()
-        {
-            return Path.Combine(Util.RootFolder(), "mods", "tile_texture_packs");
-        }
+		public static string GetExternalSavePath()
+		{
+			return Path.Combine(Util.RootFolder(), "mods", "tile_texture_packs");
+		}
 
-        public static string GetLocalSavePath()
-        {
-            return Path.Combine(ModPath, "tiles");
-        }
+		public static string GetLocalSavePath()
+		{
+			return Path.Combine(ModPath, "tiles");
+		}
 
-        public static string ModPath { get; private set; }
+		public static string ModPath { get; private set; }
 
-        public static List<string> addonPacks;
+		public static List<string> addonPacks;
 
-        // mods can call this with reflection to register any extra packs. 
-        // make sure you do it BEFORE Db.Init. OnAllModsLoaded is the recommended entry point.
-        public static void AddPack(string pack)
-        {
-            if (addonPacks == null)
-            {
-                addonPacks = new List<string>();
-            }
+		// mods can call this with reflection to register any extra packs. 
+		// make sure you do it BEFORE Db.Init. OnAllModsLoaded is the recommended entry point.
+		public static void AddPack(string pack)
+		{
+			if (addonPacks == null)
+			{
+				addonPacks = new List<string>();
+			}
 
-            addonPacks.Add(pack);
-        }
+			addonPacks.Add(pack);
+		}
 
-        public override void OnLoad(Harmony harmony)
-        {
-            ModPath = Utils.ModPath;
-            config = new SaveDataManager<Config>(path);
-            Setup();
-            GenerateData(path);
-            harmonyInstance = harmony;
+		public override void OnLoad(Harmony harmony)
+		{
+			ModPath = Utils.ModPath;
+			config = new SaveDataManager<Config>(path);
+			Setup();
+			GenerateData(path);
+			harmonyInstance = harmony;
 
-            Log.PrintVersion(this);
+			Log.PrintVersion(this);
 
-            base.OnLoad(harmony);
-        }
+			base.OnLoad(harmony);
+		}
 
-        public static void SaveConfig()
-        {
-            config.Write(true);
-        }
+		public static void SaveConfig()
+		{
+			config.Write(true);
+		}
 
-        private void Setup()
-        {
-            BlockTileRendererPatch.GetRenderInfoLayerMethod = AccessTools.Method(typeof(BlockTileRenderer), "GetRenderInfoLayer", new Type[] { typeof(bool), typeof(SimHashes) });
-            BlockTileRendererPatch.GetRenderLayerForTileMethod = AccessTools.Method(typeof(BlockTileRendererPatch), "GetRenderLayerForTile", new Type[] { typeof(RenderInfoLayer), typeof(BuildingDef), typeof(SimHashes) });
-        }
+		private void Setup()
+		{
+			BlockTileRendererPatch.GetRenderInfoLayerMethod = AccessTools.Method(typeof(BlockTileRenderer), "GetRenderInfoLayer", new Type[] { typeof(bool), typeof(SimHashes) });
+			BlockTileRendererPatch.GetRenderLayerForTileMethod = AccessTools.Method(typeof(BlockTileRendererPatch), "GetRenderLayerForTile", new Type[] { typeof(RenderInfoLayer), typeof(BuildingDef), typeof(SimHashes) });
+		}
 
-        private void GenerateData(string path)
-        {
+		private void GenerateData(string path)
+		{
 #if DATAGEN
             var root = FileUtil.GetOrCreateDirectory(Path.Combine(path, "tiles"));
             new Datagen.PackDataGen(root);
             new Datagen.TileDataGen(root);
 #endif
-        }
-    }
+		}
+	}
 }
--- TrueTiles/ModAssets.cs
+++ TrueTiles/ModAssets.cs
@@ -3,24 +3,24 @@ using UnityEngine;
 
 namespace TrueTiles
 {
-    public class ModAssets
-    {
-        public static class Prefabs
-        {
-            public static GameObject settingsDialog;
-        }
+	public class ModAssets
+	{
+		public static class Prefabs
+		{
+			public static GameObject settingsDialog;
+		}
 
-        public static class Tags
-        {
-            public static readonly Tag texturedTile = TagManager.Create("truetiles_texturedTile");
-        }
+		public static class Tags
+		{
+			public static readonly Tag texturedTile = TagManager.Create("truetiles_texturedTile");
+		}
 
-        public static void LateLoadAssets()
-        {
-            var bundle = FUtility.FAssets.LoadAssetBundle("truetilesassets");
+		public static void LateLoadAssets()
+		{
+			var bundle = FUtility.FAssets.LoadAssetBundle("truetilesassets");
 
-            Prefabs.settingsDialog = bundle.LoadAsset<GameObject>("SettingsDialog");
-            new TMPConverter().ReplaceAllText(Prefabs.settingsDialog);
-        }
-    }
+			Prefabs.settingsDialog = bundle.LoadAsset<GameObject>("SettingsDialog");
+			TMPConverter.ReplaceAllText(Prefabs.settingsDialog);
+		}
+	}
 }
