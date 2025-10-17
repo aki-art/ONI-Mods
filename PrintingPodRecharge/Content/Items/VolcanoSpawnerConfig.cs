7555000b8468606f7c3807e001fe14a040b5bd09
Aki Senkinn <akitary.art@gmail.com>
2025-10-17 22:06:38 +0200

    fix volcano crash for TI

---
+++ PrintingPodRecharge/Content/Items/VolcanoSpawnerConfig.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+
+namespace PrintingPodRecharge.Content.Items
+{
+	public class VolcanoSpawnerConfig : IEntityConfig
+	{
+		public const string ID = "PrintingPodRecharge_VolcanoSpawner";
+
+		public GameObject CreatePrefab()
+		{
+			var prefab = EntityTemplates.CreateLooseEntity(
+				ID,
+				global::STRINGS.CREATURES.SPECIES.GEYSER.SMALL_VOLCANO.NAME,
+				global::STRINGS.CREATURES.SPECIES.GEYSER.SMALL_VOLCANO.DESC + "...",
+				100,
+				true,
+				Assets.GetAnim("geyser_molten_volcano_small_kanim"),
+				"idle",
+				Grid.SceneLayer.Ore,
+				EntityTemplates.CollisionShape.RECTANGLE,
+				isPickupable: true);
+
+			return prefab;
+		}
+
+		public string[] GetDlcIds() => null;
+
+		public void OnPrefabInit(GameObject inst) { }
+
+		public void OnSpawn(GameObject inst)
+		{
+			FUtility.Utils.Spawn("GeyserGeneric_" + GeyserGenericConfig.SmallVolcano, inst);
+			Util.KDestroyGameObject(inst);
+		}
+	}
+}
